a10374f0fe87dce6b11349e3e246143b5129ef25
bowei33 <a0972944049@gmail.com>
2021-12-02 19:32:53 +0800

    py1 11/25

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/e219/teat/src
-# Build directory: /home/e219/teat/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("example")
