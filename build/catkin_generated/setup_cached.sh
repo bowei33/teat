947c87aee3b4139f494f50a6b056be450e4c8fbf
bowei33 <a0972944049@gmail.com>
2021-12-22 19:47:25 +0800

    C4 publish subscrible

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/iclab-public/teat/devel:$CMAKE_PREFIX_PATH"
 export PWD='/home/iclab-public/teat/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/iclab-public/teat/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/iclab-public/teat/src:$ROS_PACKAGE_PATH"
